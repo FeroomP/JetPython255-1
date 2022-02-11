3cd76c31931c8b4e23c94c846a3252bd137f2e07
FeroomP <danilaavdeev7@gmail.com>
2022-02-11 18:33:45 +0300

    Шаблон выполнения задания

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from math import *
 
-def fig(arg1):
+def fig4(arg1):
     s = 4 * pi * (arg1 ** 2)
     return s
 
 def main():
     n = int(input("Выберите, площадь какой фигуры вы хотите узнать?\n 1-куб\n 2-параллелепипед\n 3-цилиндр\n 4-шар\n n = "))
 
-    if n == 1:
+    if n == 4:
         a = float(input("Введите радиус шара = "))
-        print("Площадь шара с радиусом = ", a, "равна 4πR² ", fig1(a))
+        print("Площадь шара с радиусом = ", a, "равна 4πR² ", fig4(a))
 
 if __name__ == '__main__':
     main()
